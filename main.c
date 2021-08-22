34ce637baac06a716da1d5c50d37784f4ca31d27
Federico <fdrcsola@gmail.com>
2021-08-22 18:00:04 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <conio.h>
+#include <ctype.h>
 
 int main()
 {
-    int a = 0;
+    FILE* entrada = fopen ("entrada.txt","r");
+    FILE* salida = fopen ("salida.txt","wt");
+
+    if (entrada == NULL)
+    {
+        printf("\nError de apertura del archivo. \n\n");
+    }
+    else
+    {
+        
+    }
 
     return 0;
 }
